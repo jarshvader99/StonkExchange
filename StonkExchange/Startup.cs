817df9eaca7092b767f2662e24c0620b686a4e3e
jarshvader99 <jarshvader@gmail.com>
2021-02-04 16:42:47 -0600

    Add project files.

---
+++ StonkExchange/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(StonkExchange.Startup))]
+
+namespace StonkExchange
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
